cb8133f42de92fd32df8b081ddb2b133ac2f4774
Diana Laforteza <diana.laforteza@gmail.com>
2014-05-10 02:41:04 -0700

    Side Scroller and Opening Cutscene

--- Preloader.js
+++ Preloader.js
@@ -8,11 +8,35 @@ BasicGame.Preloader.prototype = {
 		this.preloadBar.x = this.preloadBar.x - (this.preloadBar.width / 2);
 		this.load.setPreloadSprite(this.preloadBar);
 		
+		// Menus
+		this.game.load.image('title_screen', 'assets/title_screen.png');
+		this.game.load.image('main_menu_bg', 'assets/main_menu.png');
+		this.game.load.image('jump_button', 'assets/main_menu/jumpbutton.png');
+		this.game.load.image('jump_button_locked', 'assets/main_menu/jumpbuttonlocked.png');
+		this.game.load.image('race_button', 'assets/main_menu/racebutton.png');
+		this.game.load.image('race_button_locked', 'assets/main_menu/racebuttonlocked.png');
+		this.game.load.image('sidescroll_button', 'assets/main_menu/sidescrollbutton.png');
+		this.game.load.image('sidescroll_button_locked', 'assets/main_menu/sidescrollbuttonlocked.png');
+		this.game.load.image('boss_button', 'assets/main_menu/bossbutton.png');
+		this.game.load.image('boss_button_locked', 'assets/main_menu/bossbuttonlocked.png');
+		this.game.load.image('resume_button', 'assets/main_menu/resumebutton.png');
+		this.game.load.image('resume_button_locked', 'assets/main_menu/resumegray.png');
+		this.game.load.image('story_button', 'assets/main_menu/storybutton.png');
+		
 		// Backgrounds
 		this.game.load.image('bg_shroom', 'assets/backgrounds/bg_shroom_lg.png');
 		this.game.load.image('bg_castle', 'assets/backgrounds/bg_castle_lg.png');
 		this.game.load.image('bg_grasslands', 'assets/backgrounds/bg_grasslands_lg.png');
+		this.game.load.image('bg_green2', 'assets/racing_game/bg_green2.png');
+		this.game.load.image('bg_green3', 'assets/racing_game/bg_green3.png');
 		this.game.load.image('bg_boss', 'assets/boss_game/bg_boss.png');
+
+		// Instructions
+		this.game.load.image('boss_instructions', 'assets/instructions/bossinstructions.png');
+		this.game.load.image('jump_instructions', 'assets/instructions/jumpinstructions.png');
+		this.game.load.image('race_instructions', 'assets/instructions/raceinstructions.png');
+		this.game.load.image('scroller_instructions', 'assets/instructions/scrollerinstructions.png');
+		this.game.load.image('score_board', 'assets/score_board.png');
 		
 		// User interfaces
 		this.game.load.spritesheet('button', 'assets/buttons.png', 193, 71);
@@ -21,6 +45,8 @@ BasicGame.Preloader.prototype = {
 		this.game.load.atlasXML('green_buttons', 'assets/ui/greenSheet.png', 'assets/ui/greenSheet.xml');
 		this.game.load.atlasXML('yellow_buttons', 'assets/ui/yellowSheet.png', 'assets/ui/yellowSheet.xml');
 		this.game.load.image('pause_icon', 'assets/ui/pause_icon.png');
+		this.game.load.image('black_screen', 'assets/black.png');
+		this.game.load.image('white_screen', 'assets/white_screen.png');
 		
 		// Player sprites
 		this.game.load.atlasXML('zizo', 'assets/aliens/alienGreen.png', 'assets/aliens/alienGreen.xml');
@@ -28,17 +54,59 @@ BasicGame.Preloader.prototype = {
 		this.game.load.atlasXML('alienBeige', 'assets/aliens/alienBeige.png', 'assets/aliens/alienBeige.xml');
 		this.game.load.atlasXML('alienBlue', 'assets/aliens/alienBlue.png', 'assets/aliens/alienBlue.xml');
 		this.game.load.atlasXML('alienYellow', 'assets/aliens/alienYellow.png', 'assets/aliens/alienYellow.xml');
+		this.game.load.atlasXML('enemies', 'assets/side_scroller/enemies.png', 'assets/side_scroller/enemies.xml');
+		this.game.load.atlasXML('items', 'assets/side_scroller/items_spritesheet.png', 'assets/side_scroller/items_spritesheet.xml');
+		this.game.load.spritesheet('other_items', 'assets/side_scroller/other_items.png', 70, 69);
+		this.game.load.image('door1', 'assets/door_openTop.png');
+		this.game.load.image('door2', 'assets/door_openMid.png');
+		this.game.load.spritesheet('km', 'assets/aliens/km.png', 75, 98);
 		
 		// Tilesets
-		this.game.load.image('base_tileset', 'assets/tiles_spritesheet.png');
+		this.game.load.image('base_tileset', 'assets/tiles_spritesheet2.png');
 		
 		// Audio
 		this.game.load.audio('racing_background_music', 'assets/racing_game/34_Chariot - Stage 4.mp3');
+		this.game.load.audio('scroller_background_music', 'assets/side_scroller/Decktonic_-_09_-_Night_Drive.ogg');
+		this.game.load.audio('splat_sound', 'assets/common_sounds/87535__flasher21__splat.wav');
 		this.game.load.audio('win_sound', 'assets/common_sounds/Kids Cheering-SoundBible.com-681813822.mp3');
 		this.game.load.audio('lose_sound', 'assets/common_sounds/Sad-Trombone.mp3');
 		this.game.load.audio('right_answer_sound', 'assets/common_sounds/right_answer_ding.mp3');
 		this.game.load.audio('wrong_answer_sound', 'assets/common_sounds/Banana Peel Slip Zip-SoundBible.com-803276918.mp3');
 		this.game.load.audio('boss_background_music', 'assets/boss_game/Battle_Special.mp3');
+	
+		// Story Opener files
+		this.game.load.image('cutscene_opener_bg', 'assets/cutscenes/open/bg_scene1.png');
+		this.game.load.image('cutscene_opener_addi_1', 'assets/cutscenes/open/addi_dialogue_0.png');
+		this.game.load.image('cutscene_opener_big_tree', 'assets/cutscenes/open/bigtree.png');
+		this.game.load.image('cutscene_opener_km_new', 'assets/cutscenes/open/km_new.png');
+		this.game.load.image('cutscene_opener_km_2', 'assets/cutscenes/open/km_new_2.png');
+		this.game.load.image('cutscene_opener_km_positive', 'assets/cutscenes/open/km_positive.png');
+		this.game.load.image('cutscene_opener_zizo_1', 'assets/cutscenes/open/zizo_dialogue_1.png');
+		this.game.load.image('cutscene_opener_zizo_2', 'assets/cutscenes/open/zizo_dialogue_2.png');
+		this.game.load.image('cutscene_opener_zizo_3', 'assets/cutscenes/open/z_youwont.png');
+		this.game.load.image('cutscene_opener_zizo_stronghold', 'assets/cutscenes/open/z_subtraction.png');
+		this.game.load.image('cutscene_opener_zizo_hmwhatsthis', 'assets/cutscenes/open/z_hmwhatsthis.png');
+		this.game.load.image('cutscene_opener_zizo_million', 'assets/cutscenes/open/z_million.png');
+		this.game.load.image('cutscene_opener_zizo_perfect', 'assets/cutscenes/open/z_perf.png');
+		this.game.load.image('cutscene_opener_newspaper', 'assets/cutscenes/open/newspaper.png');
+		this.game.load.image('cutscene_opener_newspaper2', 'assets/cutscenes/open/newspaper2.png');
+		this.game.load.audio('story_Addi8+5', 'assets/cutscenes/open/Addi8+5.mp3');
+		this.game.load.audio('story_AddiNo', 'assets/cutscenes/open/AddiNo.mp3');
+		this.game.load.audio('story_KmAreYouPositive', 'assets/cutscenes/open/KmAreYouPositive.mp3');
+		this.game.load.audio('story_KmPlusPlusPlus', 'assets/cutscenes/open/KmPlusPlusPlus.mp3');
+		this.game.load.audio('story_KmZizoMyBoy', 'assets/cutscenes/open/KmZizoMyBoy.mp3');
+		this.game.load.audio('story_NButLittleDoTheyKnow', 'assets/cutscenes/open/NButLittleDoTheyKnow.mp3');
+		this.game.load.audio('story_NOurStoryBegins', 'assets/cutscenes/open/NOurStoryBegins.ogg');
+		this.game.load.audio('story_NOurHeroZizo', 'assets/cutscenes/open/NOurHeroZizo.ogg');
+		this.game.load.audio('story_Z4+7', 'assets/cutscenes/open/Z4+7.ogg');
+		this.game.load.audio('story_Zperfect', 'assets/cutscenes/open/Zperfect.ogg');
+		this.game.load.audio('story_Zsubstrong', 'assets/cutscenes/open/Zsubstrong.ogg');
+		this.game.load.audio('story_Zwontgetaway', 'assets/cutscenes/open/Zwontgetaway.ogg');
+		this.game.load.audio('story_Zwhatdo', 'assets/cutscenes/open/Zwhatdo.ogg');
+		this.game.load.audio('story_Zwhatsthis', 'assets/cutscenes/open/Zwhatsthis.ogg');
+		this.game.load.audio('story_Zmillionmiles', 'assets/cutscenes/open/Zmillionmiles.ogg');
+		this.game.load.audio('story_swish', 'assets/cutscenes/open/swish.ogg');
+		this.game.load.audio('story_wind', 'assets/cutscenes/open/wind.ogg');
 	},
 	
 	create: function() {
@@ -48,9 +116,13 @@ BasicGame.Preloader.prototype = {
 		//	Once the load has finished we disable the crop because we're going to sit in the update loop for a short while as the music decodes
 		this.preloadBar.cropEnabled = false;
 		
-		// this.game.state.start('TitleScreen');
+		
+		
+		this.game.state.start('TitleScreen');
+		// this.game.state.start('MainMenu');
 		// this.game.state.start('SideScrollerGame');
 		// this.game.state.start('HorseGame');
-		this.game.state.start('BossGame');
+		// this.game.state.start('PlatformGame');
+		// this.game.state.start('StoryOpen');
 	}
 };
--- StoryScenes.js
+++ StoryScenes.js
@@ -1,4 +1,222 @@
+var text_style = {font: '50px kenvector_future', fill: 'black', align: 'left'};
+
 /* Scene #1 -- story introduction, before first mini game */
+BasicGame.StoryOpen = function (game) {
+	this.state_label = 'StoryOpen';
+	this.lines = [];
+	this.current_line_index = 0;
+	this.current_line = null;
+	this.zizo = null;
+	this.addi = null;
+	this.km = null;
+	this.kma = null;
+	this.tree = null;
+	this.bubble = null;
+	this.part_sprites = null;
+	this.parts = [];
+	this.current_part = 0;
+};
+
+BasicGame.StoryOpen.prototype = {
+	preload: function() {
+	},
+	
+	create: function() {
+		this.game.add.sprite(0, 0, 'cutscene_opener_bg');
+		
+		this.zizo = this.game.add.sprite(600, 525, 'zizo');
+		this.zizo.anchor.setTo(1, 1);
+		this.zizo.frame = 6;
+		
+		this.addi = this.game.add.sprite(700, 525, 'addi');
+		this.addi.anchor.setTo(0, 1);
+		this.addi.frame = 6;
+		this.addi.scale.x = -1;
+		
+		this.km = this.game.add.sprite(1170, 380, 'km');
+		this.km.animations.add('mad', [1], 1, false);
+		this.km.anchor.setTo(0, 1);
+		this.km.scale.x = -1;
+		this.km.angle = 30;
+		this.km.frame = 7;
+		
+		this.tree = this.game.add.sprite(1000, 265, 'cutscene_opener_big_tree');
+		
+		this.part_sprites = this.game.add.group();
+		
+		this.parts = this.createParts();
+		
+		// this.current_part=10;
+		this.playNextPart();
+	},
+	
+	narrator: function(string) {
+		return this.game.add.text(50, 50, string, text_style);
+	},
+	
+	playLine: function(key) {
+		this.current_line = this.game.add.audio(key);
+		this.current_line.onStop.add(this.playNextPart, this);
+		this.current_line.play();
+	},
+	
+	createParts: function() {
+		return [
+			'',
+			function() {
+				this.add_sprite(0, 0, 'white_screen');
+				this.part_sprites.add(this.narrator('Our story begins on a bright\n\t\tsummer day in Calculand'));
+				this.playLine('story_NOurStoryBegins');
+			},
+			function() {
+				var white = this.add_sprite(0, 0, 'white_screen');
+				fade_white = this.game.add.tween(white).to({alpha: 0}, 3000, null, true);
+				this.part_sprites.add(this.narrator('Our hero Zizo and his best friend\n\t\t\tAddi are hanging out,\n\t\t\t\t\t\tdoing math problems...'));
+				this.playLine('story_NOurHeroZizo');
+			},
+			function() {		
+				this.part_sprites.add(this.game.add.sprite(200, 270, 'cutscene_opener_zizo_1'));
+				this.playLine('story_Z4+7');
+			},
+			function() {
+				this.part_sprites.add(this.game.add.sprite(650, 270, 'cutscene_opener_addi_1'));
+				this.playLine('story_Addi8+5');
+			},
+			function() {
+				this.part_sprites.add(this.narrator('But little do they know,\n\t\tthey\'re being watched by\n\t\t\t\t\t\tthe evil King Minus'));
+				this.playLine('story_NButLittleDoTheyKnow');
+			},
+			function() {
+				this.part_sprites.add(this.game.add.sprite(760, 90, 'cutscene_opener_km_new'));
+				this.playLine('story_KmPlusPlusPlus');
+			},
+			function() {
+				var swish = this.game.add.audio('story_swish');
+				this.km.frame = 6;
+				this.km.angle = 0;
+				this.km.bringToTop();
+				this.kma = this.game.add.group();
+				this.kma.add(this.addi);
+				this.kma.add(this.km);
+				var km_jump = this.game.add.tween(this.km).to( { x: 750, y: 525 }, 700, Phaser.Easing.Exponential.out, false, 100);
+				km_jump.onComplete.add(function(){
+					this.km.frame = 3;
+					this.km.scale.x = 1;
+					this.addi.frame = 4;
+					this.addi.scale.x = 1;
+					var km_grab = this.game.add.tween(this.kma).to({x: 400}, 500, Phaser.Easing.Exponential.out, false, 100);
+					km_grab.onComplete.add(function(){
+						this.zizo.frame = 4;
+						this.part_sprites.add(this.game.add.sprite(200, 270, 'cutscene_opener_zizo_2'));
+						this.playLine('story_Zwhatdo');
+					}, this);
+					km_grab.start();
+					swish.play();
+				}, this);
+				km_jump.start();
+				swish.play();
+			},
+			function(){
+				this.addi.scale.x = -1;
+				this.km.scale.x = -1;
+				this.km.frame = 0;
+				this.km.bringToTop();
+				this.add_sprite(650, 150, 'cutscene_opener_km_2');
+				this.playLine('story_KmZizoMyBoy');
+			},
+			function(){
+				this.add_sprite(200, 270, 'cutscene_opener_zizo_3');
+				this.playLine('story_Zwontgetaway');
+			},
+			function(){
+				this.add_sprite(760, 265, 'cutscene_opener_km_positive');
+				this.playLine('story_KmAreYouPositive');
+			},
+			function(){
+				var swish = this.game.add.sound('story_swish');
+				var km_leave = this.game.add.tween(this.kma).to({x:1300}, 500, Phaser.Easing.Exponential.out, false);
+				km_leave.onComplete.add(this.playNextPart, this);
+				km_leave.start();
+				swish.play();
+			},
+			function(){
+				this.add_sprite(170, 230, 'cutscene_opener_zizo_stronghold');
+				this.playLine('story_Zsubstrong');
+			},
+			function(){
+				this.add_sprite(170, 230, 'cutscene_opener_zizo_million');
+				this.playLine('story_Zmillionmiles');
+			},
+			function(){
+				var wind = this.game.add.audio('story_wind');
+				var newspaper = this.add_sprite(this.game.world.centerX + 10, 0, 'cutscene_opener_newspaper');
+				newspaper.anchor.setTo(0.5, 1);
+				var paper_fall = this.game.add.tween(newspaper).to({y: 520}, 2000, Phaser.Easing.Bounce.Out, false);
+				paper_fall.onComplete.add(function(){
+					this.add_sprite(200, 270, 'cutscene_opener_zizo_hmwhatsthis');
+					this.playLine('story_Zwhatsthis');
+				}, this);
+				paper_fall.start();
+				wind.play();
+			},
+			function(){
+				var newspaper = this.add_sprite(this.game.world.centerX, this.game.world.centerY, 'cutscene_opener_newspaper2');
+				newspaper.anchor.setTo(0.5, 0.5);
+				// this.playLine('story_Znewspaper');
+				this.next();
+			},
+			function(){
+				this.zizo.frame = 0;
+				this.add_sprite(200, 270, 'cutscene_opener_zizo_perfect');
+				this.playLine('story_Zperfect');
+			},
+			function(){
+				var black = this.add_sprite(0, 0, 'black_screen');
+				black.alpha = 0;
+				var fade_black = this.game.add.tween(black).to({alpha: 1}, 1000, Phaser.Easing.Exponential.out, false);
+				fade_black.onComplete.add(this.playNextPart, this);
+				fade_black.start();
+			}
+		];
+	},
+	
+	add_sprite: function(x, y, key) {
+		return this.part_sprites.add(this.game.add.sprite(x, y, key));
+	},
+	
+	next: function() {
+		this.game.input.onDown.add(this.playNextPart, this);
+	},
+	
+	playNextPart: function() {
+		this.removeSprites();
+		this.current_part++;
+		
+		if (this.current_part >= this.parts.length) {
+			console.log('here');
+			this.game.goToNextState.call(this);
+		} else {		
+			console.log(this.current_part + ' ' + this.parts.length);
+			this.parts[this.current_part].call(this);
+		}
+	},
+	
+	removeSprites: function() {
+		this.part_sprites.destroy();
+		this.part_sprites = this.game.add.group();
+	},
+	
+	playNextLine: function() {
+		this.current_line = this.lines[this.current_line_index];
+		this.current_line.play();
+		this.current_line_index++;
+	},
+	
+	update: function() {
+	}
+};
+
+/* Scene #1 -- between first and second mini game */
 BasicGame.StoryScene1 = function (game) {
 	this.state_label = 'StoryScene1';
 };
@@ -8,8 +226,8 @@ BasicGame.StoryScene1.prototype = {
 	},
 	
 	create: function() {
-		this.game.add.text(0, 0, 'Story scene #1 -- intro before game #1', {font: '65px arial', fill: '#fff'});
-		console.log('story scene #1');
+		this.game.add.text(0, 0, 'Story scene #1 -- \nbetween game #1 and game #2', {font: '65px arial', fill: '#fff'});
+		console.log('story scene #2');
 		this.game.input.onDown.add(this.game.goToNextState, this);
 	},
 	
